4c2e01a735edeaf407a776d2561fe3704fa2c2c3
Kiichi <darkaces272@gmail.com>
2023-10-15 23:45:22 -0700

    Hair Adjustments

--- Assets/Scripts/HairPhysics.cs
+++ Assets/Scripts/HairPhysics.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tail : MonoBehaviour
+public class HairPhysics : MonoBehaviour
 {
     public int length;
     public float smoothSpeed;
